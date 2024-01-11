aaee55ebf3ff9ace3fb801ab65c455342978e000
Zeus <elderfieldzeus24@gmail.com>
2024-01-11 15:31:42 +0800

    adding elements to DOM

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -48,7 +48,31 @@ const lastLetter = (string) => {
     return last;
 }
 
+
+//ADDING ELEMENTS TO THE DOM
 console.log(add7(i));
 console.log(multiply(j, k));
 console.log(capitalize("hakdog"));
 console.log(lastLetter("meow"));
+const container = document.querySelector('body');
+const red = document.createElement('p');
+const blue = document.createElement('h3');
+const black = document.createElement('div');
+const head1= document.createElement('h1');
+const par = document.createElement('p');
+
+blue.style.cssText = ['color: blue;'];
+red.style['color'] = 'red'; 
+black.style.cssText = ['border: 2px solid black;'];
+black.style['background-color'] = 'pink';
+
+blue.textContent = "I'm a blue h3!";
+red.textContent = "Hey I'm red!";
+head1.textContent = "I'm in a div";
+par.textContent = "ME TOO!";
+
+container.appendChild(red);
+container.appendChild(blue);
+container.appendChild(black);
+black.appendChild(par);
+black.insertBefore(head1, par);
