4c6e77de99836785520941659e36432d2b3a2503
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-05-26 16:26:45 +0800

    added node.js

--- node_basic_infosite_express/public/node.js
+++ node_basic_infosite_express/public/node.js
@@ -48,7 +48,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get(['/', '/index'], (req, res) => {
     console.log(req.url);
